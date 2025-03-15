b11dce4a9b23da651af34720ad2aa378698fab5a
Max Koon <theakman96@gmail.com>
2025-03-14 21:51:52 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import { BlogPosts } from "app/components/posts";
 export default function Page() {
   return (
     <section>
-      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">koon.us</h1>
-      <p className="mb-4">{`My name is Max Koon, welcome to my website.`}</p>
+      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">My name is Max Koon, welcome to my website.</h1>
+      <p className="mb-4">{`First of all, I love Jesus and I want you to know Him. He has changed my life forever, and His love has the power to change you.\n\nI am a student at Clemson University studying Computer Science and Chinese, and am currently looking for somewhere to work this summer.`}</p>
       <div className="my-8">
         <BlogPosts />
       </div>
