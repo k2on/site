3d4ca0bf45ec8a876edef7fa740e63cef25db840
Max Koon <theakman96@gmail.com>
2025-03-19 10:21:01 -0500

    feat: add new app

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -6,54 +6,67 @@ export const metadata = {
   description: 'My past work.',
 }
 
+const WORK: Item[] = [
+    {
+        title: "Car Pooling System",
+        link: "https://github.com/k2on/elytrarides",
+        image: "/work/elytra-rides/rider.png"
+    },
+    {
+        title: "Online Shopping App",
+        link: "https://apps.apple.com/us/app/coveted/id6475173819",
+        image: "/work/coveted/screenshot-portrait.png"
+    },
+    {
+        title: "Point Tracking System",
+        link: "https://apps.apple.com/ca/app/elytra-social/id6671938478",
+        image: "/work/elytra-social/elytra-social-portrait.png"
+    },
+]
+
 export default function Page() {
   return (
     <section>
       <h1 className="font-semibold text-2xl mb-8 tracking-tighter">Portfolio</h1>
-
       <div className="grid md:grid-cols-2 gap-4">
-          <Link 
-            target="_blank" 
-            href="https://github.com/k2on/elytrarides"
-            className="flex"
-          >
-            <div className="border transition hover:bg-zinc-900 border-zinc-700 rounded-[40px] py-4 w-full flex flex-col h-full">
-              <div className="flex-1 flex items-center justify-center">
-                <Image 
-                  className="drop-shadow mx-auto object-contain" 
-                  alt="Coveted App" 
-                  src="/work/elytra/rider.png" 
-                  width={200} 
-                  height={200} 
-                />
-              </div>
-              <div className="mt-auto py-4">
-                <h1 className="text-white text-center text-xl font-semibold">Car Pooling System</h1>
-              </div>
-            </div>
-          </Link>
-          <Link 
-            target="_blank" 
-            href="https://apps.apple.com/us/app/coveted/id6475173819"
-            className="flex"
-          >
-            <div className="border transition hover:bg-zinc-900 border-zinc-700 rounded-[40px] py-4 w-full flex flex-col h-full">
-              <div className="flex-1 flex items-center justify-center">
-                <Image 
-                  className="drop-shadow mx-auto object-contain" 
-                  alt="Coveted App" 
-                  src="/work/coveted/screenshot-portrait.png" 
-                  width={200} 
-                  height={200} 
-                />
-              </div>
-              <div className="mt-auto py-4">
-                <h1 className="text-white text-center text-xl font-semibold">Online Shopping App</h1>
-              </div>
-            </div>
-          </Link>
-        </div>
-
+        {WORK.map(item => <Showcase key={item.title} item={item} />)}
+      </div>
     </section>
   )
+}
+
+type Item = {
+    title: string;
+    link: string;
+    image: string;
+}
+
+type ShowcaseProps = {
+    item: Item;
+};
+function Showcase({ item }: ShowcaseProps) {
+    const { title, link, image } = item;
+
+    return (
+      <Link 
+        target="_blank" 
+        href={link}
+        className="flex"
+      >
+        <div className="border transition bg-black hover:bg-zinc-900 border-zinc-700 rounded-[40px] py-4 w-full flex flex-col h-full">
+          <div className="flex-1 flex items-center justify-center">
+            <Image 
+              className="drop-shadow mx-auto object-contain" 
+              alt={title} 
+              src={image} 
+              width={200} 
+              height={200} 
+            />
+          </div>
+          <div className="mt-auto py-4">
+            <h1 className="text-white text-center text-xl font-semibold">{title}</h1>
+          </div>
+        </div>
+      </Link>
+    );
 }
