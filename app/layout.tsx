a53c1f723e5dee05719f259fc7ae1bdb7f8c9268
Max Koon <theakman96@gmail.com>
2025-05-28 10:50:29 -0400

    feat: favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,27 @@ export const metadata: Metadata = {
     default: "Max Koon",
     template: "%s | Max Koon",
   },
+  icons: {
+      icon: [
+          {
+            rel: 'icon',
+            type: 'image/png',
+            sizes: '32x32',
+            url: '/favicon/favicon-32x32.png',
+          },
+          {
+            rel: 'icon',
+            type: 'image/png',
+            sizes: '16x16',
+            url: '/favicon/favicon-16x16.png',
+          },
+          {
+            rel: 'apple-touch-icon',
+            sizes: '180x180',
+            url: '/favicon/apple-touch-icon.png',
+          },
+      ],
+  },
   description: "Max Koon's website.",
   openGraph: {
     title: "Max Koon",
