eb1128407cd2c572ba76ac93ac30525d7a610628
Max Koon <theakman96@gmail.com>
2025-05-28 10:17:25 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function Page() {
   return (
     <section>
-      <Image alt="Max Koon" source="headshot_small.JPG" />
+      <Image alt="Max Koon" src="headshot_small.JPG" />
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
         My name is Max Koon, welcome to my website.
       </h1>
