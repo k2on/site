bf9e12f24ad07337d08bdb86deea2004ec71a81f
Max Koon <theakman96@gmail.com>
2025-07-04 17:49:23 -0400

    feat: add footer

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="mb-16">
-      <p className="mt-8 text-neutral-600 dark:text-neutral-300">© Max Koon</p>
+      <p className="mt-8 text-neutral-600 dark:text-neutral-300">© {romanize(new Date().getFullYear())} Max Koon</p>
     </footer>
   );
 }
